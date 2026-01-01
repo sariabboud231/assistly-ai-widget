a7d6369964e7301181ea8bf5782451dbbcbdb4ec
sariabboud231 <sariabboud231@gmail.com>
2026-01-01 20:21:27 +0200

    Update and rename widget.js to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById("actionBtn");
+
+btn.addEventListener("click", () => {
+  alert("You clicked the button!");
+
+  // Replace this with ANY link you want
+  window.location.href = "https://example.com";
+});
